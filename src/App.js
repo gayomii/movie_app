04611a8a75d33d75eaf368a63f9ac58fe7c19284
Gayeon Lee <rkdus1006@gmail.com>
2020-04-27 15:51:12 +0900

    react component life cycle

--- src/App.js
+++ src/App.js
@@ -1,52 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const loveFood = [
-    { id : 1,
-      name : 'Kimchi',
-      script : 'Kimchi is Korean Food',
-      rating : 4.8
-    },
-    { id : 2,
-      name : 'Kimbop',
-      script : 'Kimbop is love...',
-      rating : 5.0
-    },
-    { id : 3,
-      name : 'Samgiopsal',
-      script : 'Samgiopsal is very very delicious',
-      rating : 4.9
-    },
-    { id : 4,
-      name : 'Apple',
-      script : 'Apple is Fruit',
-      //rating : 4.5
-    }
-]
+class App extends React.Component{
+  state = {
+    count: 0
+  };
+  constructor(props){
+    super(props);
+    console.log("constructor");
+  };
+  componentDidMount(){
+    console.log("component did");
+  };
 
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  script: PropTypes.string.isRequired,
-  rating: PropTypes.number
-};
-
-function Food({name, script, rating}) {
-  return (
-    <div>
-      <h3>I like {name}</h3>
-      <h6>{rating}/5.0</h6>
-      <h5>{script}</h5>
-    </div>
-  );
-}
-
-function App() {
-  return (
-    <div>
-      <h1>Hello!!</h1>
-      {loveFood.map(dish => (<Food key = {dish.id} name = {dish.name} script = {dish.script} rating = {dish.rating}/>))}
-    </div>
-  );
+  componentDidUpdate(){
+    console.log("Updated");
+  };
+  add = () => {
+    this.setState(current => ({count: current.count + 1}));
+  };
+  minus = () => {
+    this.setState(current => ({count: current.count - 1}));
+  };
+  render(){
+    console.log("hi");
+    return (
+      <div>
+        <h1>The number is : {this.state.count} </h1>
+        <button onClick = {this.add}>Add</button>
+        <button onClick = {this.minus}>Minus</button>
+      </div>
+    );
+  }
 }
 
 export default App;
