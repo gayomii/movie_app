235375a2f370f4d3548a44f41d61c18ee7185ab6
Gayeon Lee <rkdus1006@gmail.com>
2020-05-04 23:03:53 +0900

    getting ready for the router

--- src/App.js
+++ src/App.js
@@ -1,47 +1,7 @@
 import React from 'react';
-import axios from 'axios';
-import Movies from './Movie';
-import "./App.css";
 
-class App extends React.Component{
-  state = {
-    isLoading : true,
-    movies : []
-  };
-
-  getMovies = async () => {
-    const {data: {data: {movies}}} = await axios.get("https://yts.mx/api/v2/list_movies.json?sort_by=rating");
-    this.setState({movies, isLoading: false}); //movies 받아옴
-  }
-  componentDidMount(){
-    this.getMovies();
-  };
-  
-  render(){
-    const {isLoading, movies} = this.state;
-    return (
-      <section className="container">
-        {isLoading ? (<div className="loader">
-          <span className="loader__text">Loading...</span>
-        </div>
-        ) : (
-        <div className="movies">
-          {movies.map(movies => (
-            <Movies 
-              key={movies.id}
-              id={movies.id} 
-              year={movies.year} 
-              title={movies.title} 
-              summary={movies.summary} 
-              poster={movies.medium_cover_image}
-              genres={movies.genres} 
-            />
-          ))}  
-        </div>
-        )}
-      </section>
-    );
-  }
+function App(){
+    return <span>somethings</span>;
 }
 
 export default App;
